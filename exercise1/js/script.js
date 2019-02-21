5ed2a917824d2bc594b35e32090ec85e1b26f27c
wrahmadani10 <47784249+wrahmadani10@users.noreply.github.com>
2019-02-21 18:37:06 +0700

    Add files via upload

---
+++ exercise1/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
